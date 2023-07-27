1e7606414fe650853d221937745b8958f6592152
fabioxavierfx <63934595+fabioxavierfx@users.noreply.github.com>
2023-07-27 13:15:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,58 +21,55 @@ function gerarEtiqueta() {
       etiquetaSKUDiv.textContent = skuValue;
       etiquetaDescricaoDiv.textContent = descricao;
 
-      // Cria o conteúdo da etiqueta na nova janela
-      const conteudoEtiqueta = `
+    // Função para ajustar o tamanho da fonte
+    function ajustarTamanhoFonte(texto, elemento) {
+        const tamanhoMaximo = elemento.offsetWidth; // Largura máxima do elemento
+        const tamanhoIdeal = 0.1 * tamanhoMaximo; // Defina o tamanho ideal em relação à largura (ajuste conforme necessário)
+
+        // Verifica se o texto cabe no tamanho ideal
+        if (elemento.scrollWidth > tamanhoIdeal) {
+            let tamanhoFonte = 1; // Tamanho inicial da fonte (1 rem)
+            elemento.style.fontSize = tamanhoFonte + "rem";
+
+            // Aumenta o tamanho da fonte gradualmente até que o texto caiba no tamanho ideal
+            while (elemento.scrollWidth > tamanhoIdeal) {
+                tamanhoFonte -= 0.1; // Diminui o tamanho da fonte em 0.1 rem
+                elemento.style.fontSize = tamanhoFonte + "rem";
+            }
+        }
+    }
+
+    // Cria o conteúdo da etiqueta na nova janela
+    const conteudoEtiqueta = `
         <!DOCTYPE html>
         <html>
         <head>
             <title>Etiqueta</title>
-            <style>
-                body {
-                    font-family: Arial-black, sans-serif;
-                    text-align: center;
-                }
-				#etiqueta {
-					width: 15cm;
-					height: 9.5cm;
-					margin: 0 auto;
-					padding: 10px;
-					text-align: center;
-				}
-
-				#etiqueta h2 {
-					font-size: 320px;
-					margin: 0;
-					padding: 20px;
-					line-height: 75%;
-					font-weight: bold;
-				}
-
-				#etiqueta p {
-					font-size: 36px;
-					margin: 0;
-					padding: 20px;
-					line-height: 80%;
-					font-weight: bold;
-				}
-            </style>
+            <link rel="stylesheet" href="styles.css">
         </head>
         <body>
             <div id="etiqueta">
-                <h2>${skuValue}</h2>
-                <p>${descricao}</p>
+                <h2 id="etiquetaSKU">${skuValue}</h2>
+                <p id="etiquetaDescricao">${descricao}</p>
             </div>
             <script>
                 // Aguardar um curto período antes de acionar a impressão
                 setTimeout(() => {
+                    // Ajustar o tamanho da fonte após a renderização da etiqueta
+                    ajustarTamanhoFonte("${skuValue}", document.getElementById('etiquetaSKU'));
+                    ajustarTamanhoFonte("${descricao}", document.getElementById('etiquetaDescricao'));
                     window.print();
                     window.close();
                 }, 1000);
             </script>
         </body>
         </html>
-      `;
+    `;
 
+    // Abre uma nova janela para exibir a etiqueta
+    const novaJanela = window.open("", "_blank");
+    novaJanela.document.write(conteudoEtiqueta);
+}
       // Abrir uma nova janela com o conteúdo da etiqueta
       const novaJanela = window.open('', '_blank', 'width=200,height=150');
       novaJanela.document.open();
